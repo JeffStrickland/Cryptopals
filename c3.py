c54409c55e67ff505055dd5c73511fe56dd4a10d
Jeff Strickland <jeffstrickland@tutanota.com>
2021-10-01 12:02:15 -0400

    Update c3.py
    Simplified code

--- c3.py
+++ c3.py
@@ -9,14 +9,13 @@ Evaluate each output and choose the one with the best score.
 import codecs
 # Initial hex encoded string:
 x = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+# Hex to bytes   
+y = codecs.decode( '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736', "hex")
 
-def hex_bytes(x):  # hex > bytes 
-    return codecs.decode(x, 'hex')
-    ### b'\x1b77316?x\x15\x1b\x7f+x413=x9x(7-6<x7>x:9;76' ###
-    
 def single_xor(y): # scoring for character frequency
     strings = (''.join(chr(num ^ key) for num in y) for key in range(256))
     return (max(strings, key = lambda s: s.count(' ')))
-    
-print(single_xor(hex_bytes(x)))
+   
+# Test   
+print(single_xor(y))
 ### Cooking MC's like a pound of bacon ###
